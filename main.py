ab548d3684ff701c30491a8df7a4e2f78c41dff2
xsonykkx <sofiya.strologo@gmail.com>
2024-09-09 18:53:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,32 +4,25 @@ class Animal:
         self.fed = False
         self.name = name
 
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
 class Plant:
     def __init__(self,name):
         self.edible = False
         self.name = name
 
+
 class Mammal(Animal):
     edible = True
 
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
-
 class Predator(Animal):
     edible = True
-
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
 
 class Flower(Plant):
     edible = True
